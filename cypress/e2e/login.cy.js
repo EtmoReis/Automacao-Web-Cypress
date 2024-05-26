06bd03004833c5f3dff0999f2a7abad9b3d23421
Etmo Reis <etmo.mlic@gmail.com>
2024-05-26 14:36:18 +0100

    atualização do repositorio

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,34 @@
+/// <reference types= "cypress"/>
+const element= require("../fixtures/login.json")
+
+beforeEach(() => {
+  cy.visit('https://automacao.qacoders-academy.com.br/login');
+});
+afterEach(() => {
+  cy.screenshot()
+});
+
+  describe('Login', () => {
+    it('Login com sucesso', () => {
+        const email = Cypress.env('EMAIL');
+        const password = Cypress.env('PASSOWORD');
+        cy.Login(email, password);
+    });
+
+      it ('login com e-mail válido e senha inválida', () => {
+        cy.get(element.input_email).type(Cypress.env('EMAIL'));
+        cy.get(element.input_password).type(Cypress.env('PASSOWORD_INVALIDO'));
+        cy.get(element.btn_Login).click();
+        cy.MsgLoginFalho()
+
+      }); 
+
+      it('Login com e-mail inválido e a senha válida', () => {
+        cy.get(element.input_email).type(Cypress.env('EMAIL_INVALIDO'));
+        cy.get(element.input_password).type(Cypress.env('PASSOWORD'));
+        cy.get(element.btn_Login).click().screenshot()
+        cy.MsgLoginFalho()
+
+    });
+    
+}); 
--- cypress/e2e/spec.cy.js
+++
@@ -1,6 +0,0 @@
-describe('validar tela de Login', () => {
-  it('acessar tela de Login', () => {
-    cy.visit('https://automacao.qacoders-academy.com.br/login')
-    cy.contains("Login")
-  })
-})
